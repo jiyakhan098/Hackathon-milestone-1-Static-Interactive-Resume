dc088e83d3cd5cd4ecc41407db10e984deced1a5
javeria <jiyaprincess2905@gmail.com>
2024-09-17 06:06:47 -0700

    Changing the file name

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var toggleButton = document.getElementById("toggle-button");
 var skillsDiv = document.getElementById("skills");
 toggleButton.addEventListener("click", function () {
-  if (skillsDiv.style.display === "none") {
-    skillsDiv.style.display = "block";
-  } else {
-    skillsDiv.style.display = "none";
-  }
+    if (skillsDiv.style.display === "none") {
+        skillsDiv.style.display = "block";
+    }
+    else {
+        skillsDiv.style.display = "none";
+    }
 });
